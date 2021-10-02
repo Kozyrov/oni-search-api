b4244f8e31866521862bdc78e5036e15556b60bb
Kozyrov <sfrankie11@gmail.com>
2021-10-01 20:09:11 -0700

    tutorial code added and executed

--- oni-search-api/index.js
+++ oni-search-api/index.js
@@ -1,13 +1,40 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    try {
+        context.log('JavaScript HTTP trigger function processed a request.');
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        // Read incoming data
+        const name = (req.query.name || (req.body && req.body.name));
+        const sport = (req.query.sport || (req.body && req.body.sport));
+
+        // fail if incoming data is required
+        if (!name || !sport) {
+
+            context.res = {
+                status: 400
+            };
+            return;
+        }
+
+        // Add or change code here
+        const message = `${name} likes ${sport}`;
+
+        // Construct response
+        const responseJSON = {
+            "name": name,
+            "sport": sport,
+            "message": message,
+            "success": true
+        }
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseJSON,
+            contentType: 'application/json'
+        };
+    } catch(err) {
+        context.res = {
+            status: 500
+        };
+    }
 }
