09d9cf71011a30f008b53f68f32da67b43b20b13
Kozyrov <sfrankie11@gmail.com>
2021-10-21 17:26:47 -0700

    refactored axios call.

--- oni-search/index.js
+++ oni-search/index.js
@@ -1,30 +1,24 @@
 const axios = require("axios");
 
 const conductSearch = async (term) => {
-    let response;
     try {
-        await axios.get("https://kitsu.io/api/edge/anime?", {
+        return await axios.get("https://kitsu.io/api/edge/anime?", {
             "params": {
                 "filter[text]": encodeURIComponent(term)
             }
         }).then((res) => {
-            response = res.data;
+            return res.data;
         });
     } catch (err) {
         throw new Error(`search failed: ${err}`);
     }
 
-    return response;
 }
 
 module.exports = async (context, req) => {
     try {
-        context.log("oni-search processed search.");
-
-        // Read incoming data
         const value = (req.query.value || (req.body && req.body.value));
 
-        // fail if incoming data is required
         if (!value) {
 
             context.res = {
@@ -34,18 +28,15 @@ module.exports = async (context, req) => {
             return;
         };
 
-        // Add or change code here
         const result = await conductSearch(value);
 
         context.res = {
-            // status: 200, /* Defaults to 200 */
             status: 200,
             body: result,
             contentType: "application/json"
         };
 
     } catch(err) {
-        //testable without async
         context.res = {
             status: 500,
             err: err.message
